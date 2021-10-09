0e6ed275595b62ea6b7ec9954ac815679fcdeebe
KODZO A. M. Mawuena <nashmichel70@gmail.com>
2021-10-09 13:39:18 +0000

    Minor corrections

--- webwook.py
+++ webwook.py
@@ -10,7 +10,7 @@ def hello_world():  # this is the home page function that generates the page cod
     return "Hello world!"
 
 
-@app.route('/sms', methods=['POST'])
+@app.route('/sms', methods=['GET','POST'])
 def reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
@@ -22,23 +22,6 @@ def reply():
     resp.message(reply)
 
     return str(resp)
-
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    print(req)
-    sum = 0
-    query_result = req.get('queryResult')
-    num1 = int(query_result.get('parameters').get('number1'))
-    num2 = int(query_result.get('parameters').get('number2'))
-    sum = str(num1 + num2)
-    print('here num1 = {0}'.format(num1))
-    print('here num2 = {0}'.format(num2))
-    return {
-        "fulfillmentText": 'The sum of the two numbers is: ' + sum,
-        "source": "webhookdata"
-    }
 
 if __name__ == '__main__':
     app.run(debug=True)  # This line launch the program
